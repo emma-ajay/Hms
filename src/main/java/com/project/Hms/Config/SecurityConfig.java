43e36858b046d1e3086d3e8d6296d3c0cdc43d41
emma-ajay <emmanuelajayi205@gmail.com>
2022-09-19 14:32:46 +0100

    enabled cors in SecurityConfig

--- src/main/java/com/project/Hms/Config/SecurityConfig.java
+++ src/main/java/com/project/Hms/Config/SecurityConfig.java
@@ -61,8 +61,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//                .cors()
-//                .and()
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .exceptionHandling()
